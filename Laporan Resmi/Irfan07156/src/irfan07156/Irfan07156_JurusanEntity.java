eddb3399fbf8c32394dd9af3d519fa9f0e3389c9
irfanur rochman <59481862+irfanur02@users.noreply.github.com>
2020-11-11 10:24:58 +0700

    ubah yang menggunakan static

---
+++ Laporan Resmi/Irfan07156/src/irfan07156/Irfan07156_JurusanEntity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irfan07156;
+
+/**
+ *
+ * @author User
+ */
+public class Irfan07156_JurusanEntity {
+    static String [] Irfan07156_jurusan = {"Teknik Mesin", "Teknik Kimia", "Teknik Industri", "Teknik Elektro", "Teknik informatika", "Sistem Informasi"};
+}
